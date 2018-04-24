3d10525d47cfeb4fcfcb1365998b9b670a7612b8
Cris <crisuvas@gmail.com>
2018-04-23 20:34:04 -0500

    commit

--- app/src/main/java/com/cris/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/cris/tipcalculator/MainActivity.kt
@@ -2,11 +2,25 @@ package com.cris.tipcalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private val currencyFormat = NumberFormat.getCurrencyInstance()
+    private val percentFormat = NumberFormat.getPercentInstance()
+
+    private var billAmount = 0.0
+    private var percent = 0.15
+    private var amountTextView: TextView? = null
+    private var percentTextView: TextView? = null
+    private var tipTextView: TextView? = null
+    private var totalTextView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
