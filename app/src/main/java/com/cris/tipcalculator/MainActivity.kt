c093305ddc1e48a4028cc0f81b99ecac3dad0683
Cris <crisuvas@gmail.com>
2018-04-23 21:07:43 -0500

    commit

--- app/src/main/java/com/cris/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/cris/tipcalculator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.cris.tipcalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.SeekBar
 import android.widget.TextView
 import java.text.NumberFormat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher, SeekBar.OnSeekBarChangeListener {
 
     private val currencyFormat = NumberFormat.getCurrencyInstance()
     private val percentFormat = NumberFormat.getPercentInstance()
@@ -16,11 +20,41 @@ class MainActivity : AppCompatActivity() {
     private var percentTextView: TextView? = null
     private var tipTextView: TextView? = null
     private var totalTextView: TextView? = null
+    private var amountEditText: EditText? = null
+    private var percentSeekBar: SeekBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        amountTextView = findViewById(R.id.amountTextView)
+        percentTextView = findViewById(R.id.percentTextView)
+        tipTextView = findViewById(R.id.tipTextView)
+        totalTextView = findViewById(R.id.totalTextView)
+        amountEditText = findViewById(R.id.amountEditText)
+        percentTextView = findViewById(R.id.percentTextView)
+        tipTextView?.text = currencyFormat.format(0)
+        totalTextView?.text = currencyFormat.format(0)
 
+        amountEditText!!.addTextChangedListener(this)
+        percentSeekBar!!.setOnSeekBarChangeListener(this)
+
+    }
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
     }
 }
